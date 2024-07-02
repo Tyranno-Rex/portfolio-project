de65140f6f1fca78fa093701b1500037e409e75a
정은성 <80687043+Tyranno-Rex@users.noreply.github.com>
2024-07-02 17:36:39 +0900

    make basic solar system

--- cloud-web/cloud.js
+++ cloud-web/cloud.js
@@ -5,7 +5,7 @@
     var container, aspectRatio, HEIGHT, WIDTH, 
     fieldOfView, nearPlane, farPlane, mouseX, mouseY,
     windowHalfX, windowHalfY, stats, geometry, starStuff,
-    materialOptions, stars;
+    materialOptions, stars, mouseWheel, earthGroup;
 
     init();
     animate();
@@ -23,6 +23,7 @@
         farPlane = 1000;
         mouseX = 0;
         mouseY = 0;
+        mouseWheel = 0;
         windowHalfX = WIDTH / 2;
         windowHalfY = HEIGHT / 2;
 
@@ -40,7 +41,7 @@
         starForge();
 
         if (webGLSupport()) {
-            renderer = new THREE.WebGLRenderer({ alpha: true });
+            renderer = new THREE.WebGLRenderer({alpha: true});
         } else {
             renderer = new THREE.CanvasRenderer();
         }
@@ -59,8 +60,7 @@
 
         window.addEventListener('resize', onWindowResize, false);
         document.addEventListener('mousemove', onMouseMove, false);
-
-    
+        document.addEventListener('wheel', onMouseWheel, false);
     }
 
     function animate() {
@@ -72,6 +72,8 @@
     function render() {
         camera.position.x += (mouseX - camera.position.x) * 0.005;
         camera.position.y += (mouseY - camera.position.y) * 0.005;
+        camera.position.z += (mouseWheel - camera.position.z) * 0.05;
+        earthGroup.rotation.y += 0.01;
         camera.lookAt(scene.position);
         renderer.render(scene, camera);
     }
@@ -97,33 +99,66 @@
     }
 
     function starForge() {
-        var starQty = 45000;
-        geometry = new THREE.SphereGeometry(1000, 100, 50);
-
+        var starQty = 10000;
+        geometry = new THREE.SphereGeometry(1000, 32, 16);
+        
         materialOptions = {
             size : 0.1,
             transparent: true,
             opacity: 0.7
         };
-
+        
         starStuff = new THREE.PointCloudMaterial(materialOptions);
-
+        
         for (var i = 0; i < starQty; i++) {
             var starVertex = new THREE.Vector3();
             starVertex.x = Math.random() * 2000 - 1000;
             starVertex.y = Math.random() * 2000 - 1000;
             starVertex.z = Math.random() * 2000 - 1000;
-
+            
             geometry.vertices.push(starVertex);
         }
-
+        
         stars = new THREE.PointCloud(geometry, starStuff);
         scene.add(stars);
+
+
+        // 태양 생성
+        var sunGeometry = new THREE.SphereGeometry(100, 32, 32);  // 적절한 크기로 설정
+        var sunMaterial = new THREE.MeshBasicMaterial({ 
+            color: 0xff0000, 
+            transparent: true,
+            opacity: 1
+        });
+        
+        var sun = new THREE.Mesh(sunGeometry, sunMaterial);
+        sun.position.set(0, 0, 0);  // 태양의 위치를 중앙으로 설정
+        
+        
+        
+        // 지구 생성
+        var earthGeometry = new THREE.SphereGeometry(10, 32, 32);  // 적절한 크기로 설정
+        var earthMaterial = new THREE.MeshBasicMaterial({ 
+            color: 0x0000ff, 
+            transparent: true,
+            opacity: 1
+        });
+        
+        var earth = new THREE.Mesh(earthGeometry, earthMaterial);
+        earthGroup = new THREE.Group();
+        earth.position.set(200, 0, 0);  // 지구의 위치를 태양에서 200만큼 떨어진 위치로 설정
+        earthGroup.add(sun);
+        earthGroup.add(earth);
+        scene.add(earthGroup);
     }
 
     function onMouseMove(e) {
         mouseX = e.clientX - windowHalfX;
         mouseY = e.clientY - windowHalfY;
+    }
+    
+    function onMouseWheel(e) {
+        mouseWheel += e.deltaY * 0.05; // Adjust the sensitivity as necessary
     }
 
 }) ();
