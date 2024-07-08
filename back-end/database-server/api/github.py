0cac7ad891b35271327e8de6d0c9aa85748afb20
정은성 <80687043+Tyranno-Rex@users.noreply.github.com>
2024-07-08 17:42:48 +0900

    make CronServer base
    use only github api (exclude crawling method and

--- back-end/database-server/api/github.py
+++ back-end/database-server/api/github.py
@@ -2,29 +2,132 @@ from requests import get
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 
-client = MongoClient('localhost', 27017)
+# client = MongoClient('localhost', 27017)
 
-try:
-    # 연결 테스트
-    client.admin.command('ismaster')
-    db = client['github']
-    print('Connected to MongoDB')
-except ConnectionFailure:
-    print('MongoDB server not available')
+OWNER_NAE = "Tyranno-Rex"
+# try:
+#     # 연결 테스트
+#     client.admin.command('ismaster')
+#     db = client['github']
+#     print('Connected to MongoDB')
+# except ConnectionFailure:
+#     print('MongoDB server not available')
 
-token = open("./back-end/database-server/api/database/git-token.txt", "r").read().strip()
-url = "https://api.github.com/user/repos"
-headers = {
+# 1. 모든 repository의 이름과 url을 가져온다.
+token = open("C:/Users/admin/project/back/back-end/database-server/api/database/git-token.txt", "r").read().strip()
+
+url_all_repos = "https://api.github.com/user/repos"
+headers_all_repos = {
     "Accept": "application/vnd.github+json",
     "Authorization": "Bearer " + token,
     "X-GitHub-Api-Version": "2022-11-28"
 }
 
-response = get(url, headers=headers)
-json = response.json()
+response_all_repos = get(url_all_repos, headers=headers_all_repos)
+json_all_repos = response_all_repos.json()
+repo_all_list = []
 
-i = 0 
-for repo in json:
-    if (i == 3):
-        print(repo)
-    i += 1
+for json in json_all_repos:
+    repo_all_list.append({
+        "name": json["name"],
+        "url": json["html_url"]
+    })
+
+# 2. 각 repository의 README.md를 가져온다.
+import base64
+
+def decode_base64(content):
+    base64_bytes = content.encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    return message_bytes.decode('utf-8')
+
+for repo in repo_all_list:
+    if repo["name"] not in ["BE-study", "portfolio-project"]:
+        continue
+    
+    url_readme = f"https://api.github.com/repos/{OWNER_NAE}/{repo['name']}/readme"
+    headers_readme = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {token}",
+        "X-GitHub-Api-Version": "2022-11-28"
+    }
+
+    response_readme = get(url_readme, headers=headers_readme)
+
+    if response_readme.status_code == 200:
+        json_readme = response_readme.json()
+        encoded_content = json_readme["content"]
+        decoded_content = decode_base64(encoded_content)
+        repo["readme"] = decoded_content
+        project_name = decoded_content.split('PROJECT_NAME : ')[1].split('\n')[0],
+        project_description = decoded_content.split('PROJECT_DESCRIPTION : ')[1].split('\n')[0],
+        project_url = decoded_content.split('PROJECT_URL : ')[1].split('\n')[0],
+        project_complete_status = decoded_content.split('PROJECT_COMPLETION_STATUS : ')[1].split('\n')[0],
+        project_multi = decoded_content.split('PROJECT_MULTI : ')[1].split('\n')[0],
+        project_category = decoded_content.split('PROJECT_CATEGORY : ')[1].split('\n')[0],
+        project_subproject = decoded_content.split('PROJECT_SUBPROJECT : ')[1].split('\n')[0]
+
+
+        repo['description'] = project_description
+        repo['complete_status'] = project_complete_status
+        repo['multi'] = project_multi
+        project_category = project_category[0].split(', ')
+        repo['category'] = project_category
+        project_subproject = project_subproject.split(', ')
+        repo['subproject'] = project_subproject
+    
+        multi = project_multi[0]
+        
+        # 3. 각 repository에서 세부 파일에서 readme가 있는 경우 가져온다.
+        if multi == 'TRUE':
+            for sub_project in project_subproject:
+                url_sub_readme = f"https://api.github.com/repos/{OWNER_NAE}/{repo['name']}/contents/{sub_project}"
+                headers_sub_readme = {
+                    "Accept": "application/vnd.github+json",
+                    "Authorization": f"Bearer {token}",
+                    "X-GitHub-Api-Version": "2022-11-28"
+                }
+                response_sub_readme = get(url_sub_readme, headers=headers_sub_readme)
+                json_sub_readme = response_sub_readme.json()
+                for json in json_sub_readme:
+                    if (json["name"] == "README.md"):
+                        url_sub_readme = json["download_url"]
+                        response_sub_readme = get(url_sub_readme)
+                        decoded_content = response_sub_readme.text
+                        sub_project_name = decoded_content.split('PROJECT_NAME : ')[1].split('\n')[0]
+                        sub_project_description = decoded_content.split('PROJECT_DESCRIPTION : ')[1].split('\n')[0]
+                        sub_project_url = decoded_content.split('PROJECT_URL : ')[1].split('\n')[0]
+                        sub_project_complete_status = decoded_content.split('PROJECT_COMPLETION_STATUS : ')[1].split('\n')[0]
+                        sub_project_multi = decoded_content.split('PROJECT_MULTI : ')[1].split('\n')[0]
+                        sub_project_category = decoded_content.split('PROJECT_CATEGORY : ')[1].split('\n')[0]
+                        sub_project_subproject = decoded_content.split('PROJECT_SUBPROJECT : ')[1].split('\n')[0]
+                        repo_all_list.append({
+                            "name": sub_project_name,
+                            "url": sub_project_url,
+                            "readme": decoded_content,
+                            "description": sub_project_description,
+                            "complete_status": sub_project_complete_status,
+                            "multi": sub_project_multi,
+                            "category": sub_project_category,
+                            "subproject": sub_project_subproject
+                        })
+    else:
+        repo["readme"] = ""
+
+
+for repo in repo_all_list:
+    if (repo["name"] != "BE-study" and repo["name"] != "portfolio-project" 
+        and repo["name"] != "study-os" and repo["name"] != "study-server" and repo["name"] != "study-database"):
+        continue
+    print(repo["name"])
+    print(repo["url"])
+    # print(repo["readme"])
+    print(repo["description"])
+    print(repo["complete_status"])
+    print(repo["multi"])
+    print(repo["category"])
+    print(repo["subproject"])
+    print("")
+
+
+
--- back-end/database-server/cronMainServer.py
+++ back-end/database-server/cronMainServer.py
@@ -6,6 +6,8 @@ from bs4 import BeautifulSoup
 from fastapi import FastAPI, APIRouter
 from pymongo import MongoClient
 from starlette.middleware.cors import CORSMiddleware
+import random
+import time
 
 class FASTAPI_CRON_SERVER:
     def __init__(self):
@@ -23,10 +25,59 @@ class FASTAPI_CRON_SERVER:
         self.router = APIRouter()
         self.router.add_api_route('/crawling', endpoint=self.crawling_main, methods=['GET'])
         self.app.include_router(self.router)
+
+
+    def get_git_repo_with_retries(self, url, max_retries=3):
+        attempt = 0
+        while attempt < max_retries:
+            try:
+                wait_time = (2 ** attempt) + random.uniform(0, 1)  # Exponential backoff with jitter
+                if attempt == 0:
+                    print(f"Attempt {attempt} fetched {url}.")
+                else:
+                    print(f"Attempt {attempt} fetched {url}. Retrying in {wait_time:.2f} seconds...")
+                git_repo = requests.get(url)
+                time.sleep(wait_time)
+                git_repo.raise_for_status()  # Raise an exception for HTTP errors
+                return git_repo
+            except requests.RequestException as e:
+                attempt += 1
+                print(f"Failed to fetch the URL: {e}")
+        raise Exception(f"Failed to fetch the URL after {max_retries} attempts")
+
+    def get_readme_with_retries(self, soup, max_retries=3):
+        attempt = 0
+        while attempt < max_retries:
+            try:
+                wait_time = (2 ** attempt) + random.uniform(0, 1)  # Exponential backoff with jitter
+                if attempt == 0:
+                    print(f"Attempt {attempt} parsed the HTML.")
+                else:
+                    print(f"Attempt {attempt} parsed the HTML. Retrying in {wait_time:.2f} seconds...")
+                git_readme_element = soup.select_one("article.markdown-body.entry-content.container-lg")
+                time.sleep(wait_time)
+                if git_readme_element:
+                    return git_readme_element.text.strip()
+                else:
+                    raise Exception("README element not found")
+            except Exception as e:
+                attempt += 1
+                print(f"Failed to parse the HTML: {e}")
+        raise Exception(f"Failed to parse the HTML after {max_retries} attempts")
+
     def crawling_get_README(self, url):
+        # Fetch the repository page with retries
+        # git_repo = self.get_git_repo_with_retries(url)
+        # Parse the HTML content with BeautifulSoup
+        # git_soup = BeautifulSoup(git_repo.text, 'html.parser')
+        # Extract the README text with retries
+        # git_readme = self.get_readme_with_retries(git_soup)
         git_repo = requests.get(url)
         git_soup = BeautifulSoup(git_repo.text, 'html.parser')
+        print(git_soup)
         git_readme = git_soup.select_one("article.markdown-body.entry-content.container-lg").text.strip()
+        print(git_readme)
+
         project_crawling_info = git_readme.split('Project Crawling')[1].strip()
         project_name = project_crawling_info.split('PROJECT_NAME : ')[1].split('\n')[0]
         project_description = project_crawling_info.split('PROJECT_DESCRIPTION : ')[1].split('\n')[0]
@@ -53,6 +104,7 @@ class FASTAPI_CRON_SERVER:
                 "category": category,
                 "subproject_crawling_data": subproject_crawling_data
         }
+
     def crawling_main(self):
         git_url = "https://github.com/Tyranno-Rex?tab=repositories"
         response = requests.get(git_url)
@@ -63,12 +115,15 @@ class FASTAPI_CRON_SERVER:
             repo_url = f"https://github.com{repo['href']}"
             repos.append({"name": repo.text.strip(), "url": repo_url})
         
+        crawling_data = []
         for i in range(len(repos)):
-            if (i == 0):
-                project_crawling_info = self.crawling_get_README(repos[i]["url"])
-                return project_crawling_info
+            # if (repos[i]["name"] == "portfolio-project" or repos[i]["name"] == "BE-study"):
+            # if (repos[i]["name"] == "portfolio-project"):
+            if (repos[i]["name"] == "BE-study"):
+                crawling_data.append(self.crawling_get_README(repos[i]["url"]))
 
-    
+        # self.git_repo_mongodb['crawling_data'].insert_many(crawling_data)
+        return {"crawling_data": crawling_data}
 if __name__ == "__main__":
     server = FASTAPI_CRON_SERVER()
     import uvicorn
