da1d7d88e5d065144b89dc5648ff3c1c0cf5fbe6
정은성 <80687043+Tyranno-Rex@users.noreply.github.com>
2024-08-01 09:19:52 +0900

    update chatting with gpt success!
    this chatting is not allowed everyone. only allowed who get a secret code.

--- front-end/script.js
+++ front-end/script.js
@@ -13,15 +13,79 @@ function sendMessage() {
     const chatMessages = document.getElementById('chatMessages');
     chatMessages.appendChild(messageElement);
 
+    // send 버튼을 일시적으로 비활성화
+    document.getElementById('sendButton').disabled = true;
+    var question_key = localStorage.getItem('chatbotPassword');
+    console.log('question_key : ', question_key);
+    fetch('https://jeongeunseong.store/openai/api/send_question/', { // local server
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ question: message, question_key: question_key}),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        const messageElement = document.createElement('div');
+        messageElement.className = 'chat-message bot';
+        messageElement.textContent = data.answer;
+        console.log(data.answer);
+        chatMessages.appendChild(messageElement);
+    });
+
+    document.getElementById('sendButton').disabled = false;
     chatMessages.scrollTop = chatMessages.scrollHeight;
     userInput.value = '';
 }
 
 function showChat() {
-    const chatContainer = document.getElementsByClassName('chat-container')[0];
-    const showChatButton = document.getElementById('showChatButton');
-    chatContainer.style.display = 'flex';
-    showChatButton.style.display = 'none';
+    const ModalContent = `'
+        <div class="detail-title">Chatbot</div>
+        <div class="detail-description">
+        This is a chatbot that can help you with your questions.
+        </div>
+        <div class="detail-description">
+        But, I'm a poor Guy, So set a password for Chatbot.
+        </div>
+        <div class="detail-description">
+        Please enter the password to chat with me. 
+        </div>
+        <br><br>
+        <input type="password" id="password" placeholder="Enter Password">
+        <button onclick="checkPassword_Click()">Enter</button>
+    `;
+    document.getElementById('modal-content').innerHTML = ModalContent;
+    myModal.open('#myModal');
+}
+
+function savePasswordToLocalStorage() {
+    const password = document.getElementById('password').value;
+    localStorage.setItem('chatbotPassword', password);
+    console.log(localStorage.getItem('chatbotPassword'));
+}
+
+function checkPassword_Click() {
+    console.log('password : ', document.getElementById('password').value);
+    fetch('https://jeongeunseong.store/openai/check/password/', { // local server
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ password: document.getElementById('password').value}),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        if (data.data === 'Password is correct') {
+            savePasswordToLocalStorage();
+            myModal.close('#myModal');
+            const chatContainer = document.getElementsByClassName('chat-container')[0];
+            const showChatButton = document.getElementById('showChatButton');
+            chatContainer.style.display = 'flex';
+            showChatButton.style.display = 'none';
+        } else {
+            alert('Password is incorrect');
+        }
+    });   
 }
 
 function closeChat() {
@@ -31,7 +95,7 @@ function closeChat() {
     showChatButton.style.display = 'block';
 }
 
-function showIndicator() {
+function showIndicator() {    
     const indicatorContent = document.getElementsByClassName('repo-indicator-container')[0];
     const indicatorButton = document.getElementById('showIndicatorButton');
     const indicatorCloseButton = document.getElementById('repo-indicator-close');
@@ -58,3 +122,4 @@ document.getElementById('closeChatButton').addEventListener('click', closeChat);
 document.getElementById('showIndicatorButton').addEventListener('click', showIndicator);
 
 window.sendMessage = sendMessage;
+window.checkPassword_Click = checkPassword_Click;
